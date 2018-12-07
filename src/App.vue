ccbcbdc2f07f6e5fef34c59866cb994b4f0c1b1a
barnboo <250159440@qq.com>
2018-12-07 18:07:43 +0800

    添加modules

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <FmMakingForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import FmMakingForm from './components/FormGenerator/Container'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FmMakingForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
